82c8267f0d0500ec20efd431b154c128c16dc401
xusraw1 <xusrav200329@gmail.com>
2024-04-17 23:40:04 +0500

    #3 - Создал модель профиля и добавил в админ панель, провел миграции.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = [
+        'username',
+        'email',
+        'slug',
+        'gender',
+        'created',
+    ]
+    list_filter = [
+        'gender',
+        'created',
+    ]
+    list_editable = ['gender']
--- users/models.py
+++ users/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    GENDER = [
+        ('man', 'Мужчина'),
+        ('woman', 'Женщина')
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=150, null=True, blank=True, unique=True)
+    email = models.EmailField(unique=True)
+    gender = models.CharField(max_length=5, choices=GENDER, default='Неизвестно', verbose_name='Пол')
+    first_name = models.CharField(max_length=50, null=True, blank=True, verbose_name='Имя')
+    last_name = models.CharField(max_length=50, null=True, blank=True, verbose_name='Фамилия')
+
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True, verbose_name='Дата создания профиля')
+    updated = models.DateTimeField(auto_now_add=True, blank=True, null=True,
+                                   verbose_name='Дата последнего обновления профиля')
+
+    slug = models.SlugField(unique=True, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(str(self.username) + str(self.id))
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.username} + {self.email}"
+
+    class Meta:
+        ordering = ['-created']
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+        db_table = 'profile'
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, *args, **kwargs):
+    if created:
+        Profile.objects.create(
+            user=instance,
+            username=instance.username,
+            email=instance.email
+        )
