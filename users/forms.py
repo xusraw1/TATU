0a8e0d50a77304b8135c4434cbad94cef5f23f8a
xusraw1 <xusrav200329@gmail.com>
2024-04-17 23:46:27 +0500

    #4 - Создал форму для создания профиля

---
+++ users/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if len(username) >= 150:
+            raise ValidationError("Имя пользователя не должно превышать 150 символов.")
+        return username
