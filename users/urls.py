2f2828c7d4bf164dd681d2f75c5bf3869f2d1769
xusraw1 <xusrav200329@gmail.com>
2024-04-18 21:18:25 +0500

    #15 - Использовал встроенный класс для изменения пароля

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import CreateProfileView, LoginProfileView, LogoutProfile, ProfileView, ProfileChangeView
+from django.contrib.auth import views
+from django.urls import reverse_lazy
 
 app_name = 'users'
 
@@ -8,6 +10,14 @@ urlpatterns = [
     path('register/', CreateProfileView.as_view(), name='register'),
     path('login/', LoginProfileView.as_view(), name='login'),
     path('logout/', LogoutProfile.as_view(), name='logout'),
+
+    path('password-change/',
+         views.PasswordChangeView.as_view(template_name='users/registration/password_change_form.html',
+                                          success_url=reverse_lazy('users:password_change_done')),
+         name='password_change'),
+    path('password-change-done/',
+         views.PasswordChangeDoneView.as_view(template_name='users/registration/password_change_done.html'),
+         name='password_change_done'),
 
     # профиль
     path('profile/<slug:slug>/', ProfileView.as_view(), name='profile'),
