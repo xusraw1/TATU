8c452b35dbe56f13de1b673bc593f694ea83b23b
xusraw1 <xusrav200329@gmail.com>
2024-04-18 10:42:24 +0500

    #6 - Создал страницу для создания профиля, добавил сообщения и протестил.

--- users/views.py
+++ users/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
-from .forms import CreateUserForm
+from .forms import CreateUserForm, LoginForm
 from .models import Profile
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.views import View
 
 
 class CreateProfileView(CreateView):
     model = Profile
     template_name = 'users/create_profile.html'
     form_class = CreateUserForm
+    success_url = reverse_lazy('register')
+
+    def form_valid(self, form):
+        email = form.cleaned_data['email']
+
+        if Profile.objects.filter(email=email).exists():
+            messages.error(self.request, 'Пользователь с таким email уже существует.')
+            return self.form_invalid(form)
+
+        self.object = form.save()
+        messages.success(self.request, 'Профиль успешно создан!')
+
+        return self.form_invalid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Произошла ошибка при создании профиля.')
+        return super().form_invalid(form)
