f2f2ef9bb64bdc17b51b05ba4d15418654b0b73e
xusraw1 <xusrav200329@gmail.com>
2024-04-22 23:04:37 +0500

    #18 - Создал модель для пароля и добавил в admin панель, провел миграции

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+# password
+@admin.register(Password)
+class PasswordAdmin(admin.ModelAdmin):
+    list_display = ['profile', 'created', 'id']
+
--- services/models.py
+++ services/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import Profile
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Password(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True, null=True)
+    password = models.CharField(max_length=150, blank=True, null=True, verbose_name='Пароль')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.profile} | {self.rate}"
